b4bf5f6502f5290d2a8c3812d33b33035caeb513
anton <antonlarsson.musik@gmail.com>
2025-09-20 10:05:59 +0200

    tried disabling cors for next.js, not successful yet

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -6,7 +6,29 @@ const ContactForm: React.FC = () => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
 
-        
+        const form = e.target as HTMLFormElement
+		const formValues = Object.fromEntries(new FormData(form).entries())
+
+        try {
+            await fetch(process.env.NEXT_PUBLIC_BACKEND_URL || "localhost:8080/mail", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                //mode: "no-cors",
+                body: JSON.stringify(formValues)
+            }).then(
+                (response) => {
+                    if (!response.ok)
+                        throw new Error(`HTTP Error. Status: ${response.status}`)
+                    console.log(response.json())
+                    return response.json()
+                })
+
+            setSuccessMessage('Thanks for contacting me!')
+            form.reset()
+        } catch (err) {
+            console.log(err)
+            alert('an error ocurred')
+        }
     }
 
     return (
