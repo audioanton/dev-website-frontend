0eeac4bcca41737f2b4e96641f3c30ec227bcb8c
anton <antonlarsson.musik@gmail.com>
2026-01-04 11:40:40 +0100

    tinkering with logrocket blog

--- app/ui/components/Modal.tsx
+++ app/ui/components/Modal.tsx
@@ -9,14 +9,30 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ content, buttonLabel }) => {
   const [open, setOpen] = useState(false);
 
-  const toggle = () => {
-    setOpen(!open);
+  const openModal = () => {
+    setOpen(true);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.key === "Escape") {
+      closeModal();
+    }
   };
 
   return (
     <>
-      <button onClick={toggle}>{buttonLabel}</button>
-      <dialog className={styles.modal} open={open}>
+      <button onClick={openModal}>{buttonLabel}</button>
+      <dialog className={styles.modal} open={open} onKeyDown={handleKeyDown}>
+        <button
+          className={`${styles.modalCloseBtn} ${styles.modalButton}`}
+          onClick={closeModal}
+        >
+          &times;
+        </button>
         {content}
       </dialog>
     </>
