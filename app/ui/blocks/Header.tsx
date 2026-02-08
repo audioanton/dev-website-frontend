201d5d86a7feafe9215a4ddecc4100752bddf41d
anton <antonlarsson.musik@gmail.com>
2026-02-08 11:09:15 +0100

    feat: mobile menu

--- app/ui/blocks/Header.tsx
+++ app/ui/blocks/Header.tsx
@@ -8,8 +8,8 @@ interface HeaderData {
 
 function Header({ title, subtitle, titleFont }: HeaderData) {
   return (
-    <div className="absolute top-0 left-0 w-full md:w-3/4 h-[125px]">
-      <div className="absolute bottom-0 px-[50px] py-[15px] xl:px-[300px]">
+    <div className="sticky top-0 md:absolute md:left-0 w-full md:w-3/4 h-[125px]">
+      <div className="relative bottom-0 px-[50px] py-[15px] xl:px-[300px]">
         <h1
           className={`${titleFont?.className} text-neutral-400 text-shadow-lg/30 text-shadow-black text-sm font-bold [text-shadow:_1px_1px_1_#000,_-1px_-1px_1_#000,_1px_-1px_1_#000,_-1px_1px_1_#000]`}
         >
