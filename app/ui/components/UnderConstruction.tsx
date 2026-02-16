17baa7d6ab96e0c23c7d9aae259bdfd744062731
anton <antonlarsson.musik@gmail.com>
2026-02-16 08:55:41 +0100

    fix: mobile x margin

--- app/ui/components/UnderConstruction.tsx
+++ app/ui/components/UnderConstruction.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const UnderConstruction = () => {
   return (
-    <div>
+    <div className="max-w-4/5 md:max-w-1/1 mx-auto">
       <h2 className="text-2xl my-5">Oops.. This does not exist. Yet.</h2>
       <Image
         src="/kokos_laying.PNG"
